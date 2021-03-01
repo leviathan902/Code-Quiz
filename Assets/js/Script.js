4a46bb176a4a3ecb9268352f5741d91376bb0dfe
Levi H <levih902@gmail.com>
2021-02-28 21:41:35 -0600

    Added sudo-code to JS file.

---
+++ Assets/js/Script.js
@@ -0,0 +1,9 @@
+// Create a timed quiz using Javascript.
+// Needs to store high scores of multiple users.
+
+// Click start button begins timer, presents question.
+// Question is answered, presented with another question.
+// When answered incorrectly, time is removed from clock.
+// All questions answered/timer reaches 0, game is over.
+// When game ends, save initials/score.
+// Score and initials will need to persist against reload/leaving and returning to page.
